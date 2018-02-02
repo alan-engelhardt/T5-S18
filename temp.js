71d81999a6d1987025ce3744a10aa04694a805a0
Alan Engelhardt <Alan@windows-mts66k1.kea.local>
2018-02-02 15:10:32 +0100

    fetch productlist unfiltered

---
+++ temp.js
@@ -0,0 +1,19 @@
+"use strict"
+const template = document.querySelector('template').content;
+const main = document.querySelector('main');
+const link = "http://kea-alt-del.dk/t5/api/productlist";
+const imglink = "http://kea-alt-del.dk/t5/site/imgs/"
+
+//imglink + "small/" + imagename + "-sm.jpg";
+
+fetch(link).then(result=>result.json()).then(data=>show(data));
+
+function show(data){
+	data.forEach(elem=>{
+		const clone = template.cloneNode(true);
+		clone.querySelector("img").src="http://kea-alt-del.dk/t5/site/imgs/small/" + elem.image + "-sm.jpg";
+		clone.querySelector("h2").textContent=elem.name;
+		clone.querySelector("p").textContent=elem.shortdescription;
+		main.appendChild(clone);
+	})
+}
