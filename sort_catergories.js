c5c10a898271641e8920f54b77f720438a04e923
Alan Engelhardt <Alan@alansmbamid2014.home>
2018-02-05 15:13:45 +0100

    fetch plist inside car function

--- sort_catergories.js
+++ sort_catergories.js
@@ -3,28 +3,21 @@ const template = document.querySelector('template').content;
 const main = document.querySelector('main');
 const imglink = "http://kea-alt-del.dk/t5/site/imgs/"
 
-
 const catLink = "http://kea-alt-del.dk/t5/api/categories";
+const pListLink = "http://kea-alt-del.dk/t5/api/productlist";
 
-fetch(catLink).then(result=>result.json()).then(data=>sort(data));
+fetch(catLink).then(result=>result.json()).then(data=>createCats(data));
 
-function sort(data){
-	data.forEach(cat=>{
-		console.log(cat);
+function createCats(categories){
+	categories.forEach(category=>{
 		const section = document.createElement("section");
 		const h2 = document.createElement("h2");
-		section.id = cat;
-		h2.textContent = cat;
+		section.id = category;
+		h2.textContent = category;
 		section.appendChild(h2);
 		main.appendChild(section);
 	});
-	getPlist(pListLink);
-}
-
-const pListLink = "http://kea-alt-del.dk/t5/api/productlist";
-
-function getPlist(link){
-	fetch(link).then(result=>result.json()).then(data=>show(data));
+	fetch(pListLink).then(result=>result.json()).then(data=>show(data));
 }
 
 function show(data){
